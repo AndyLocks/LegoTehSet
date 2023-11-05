57b607fdad0a840a70e41e3499f77c05a394cd52
AndyLocks <jandylokc@gmail.com>
2023-11-05 23:42:05 +0100

    CommandCommandList: add javadoc

--- src/main/java/lego_teh_set_discord_bot/commands/CommandCommandList.java
+++ src/main/java/lego_teh_set_discord_bot/commands/CommandCommandList.java
@@ -1,9 +1,18 @@
 package lego_teh_set_discord_bot.commands;
 
 import lego_teh_set_discord_bot.component_creators.EmbedBuilderCreator;
+import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+/**
+ * Класс для прослушки команды command_list.
+ * Выводит список всех команд в чат.
+ * <p>
+ * Этот класс нужен для {@link net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder}
+ * Этот класс реализует {@link #onSlashCommandInteraction(SlashCommandInteractionEvent)}
+ */
 public class CommandCommandList extends ListenerAdapter {
 
     @Override
