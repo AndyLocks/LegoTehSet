0c985a43fc291afeb51d8e418b274928c2d19a41
AndyLocks <jandylokc@gmail.com>
2023-11-05 23:45:23 +0100

    SpringConfig: add javadoc

--- src/main/java/spring_config/SpringConfig.java
+++ src/main/java/spring_config/SpringConfig.java
@@ -3,6 +3,9 @@ package spring_config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Конфигурация spring framework
+ */
 @Configuration
 @ComponentScan("rebrickableAPI")
 public class SpringConfig {}
