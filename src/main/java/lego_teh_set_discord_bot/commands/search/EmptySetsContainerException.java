0fc8c6c25acd6b307b0e535b40ebdce2e214c18c
AndyLocks <jandylokc@gmail.com>
2023-11-05 23:42:51 +0100

    EmptySetsContainerException: add javadoc

--- src/main/java/lego_teh_set_discord_bot/commands/search/EmptySetsContainerException.java
+++ src/main/java/lego_teh_set_discord_bot/commands/search/EmptySetsContainerException.java
@@ -1,5 +1,8 @@
 package lego_teh_set_discord_bot.commands.search;
 
+/**
+ * Возникает в случаях, когда список в {@link SetsContainer} оказывается пустым.
+ */
 public class EmptySetsContainerException extends RuntimeException {
     public EmptySetsContainerException(String message) {
         super(message);
