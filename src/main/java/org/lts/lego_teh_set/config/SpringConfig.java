57ac3d93c308a6eae04f40510dbf8fc7fc7b337a
AndyLocks <jandylokc@gmail.com>
2024-12-23 05:28:32 +0100

    Update: code optimisation

--- src/main/java/org/lts/lego_teh_set/config/SpringConfig.java
+++ src/main/java/org/lts/lego_teh_set/config/SpringConfig.java
@@ -1,12 +1,8 @@
 package org.lts.lego_teh_set.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * Spring framework configuration.
- */
+/// Spring framework configuration.
 @Configuration
-@ComponentScan("org.lts.lego_teh_set")
 public class SpringConfig {
 }
