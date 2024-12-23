a354da1b4871ab06abb0c53c4d3b9e37007e993f
AndyLocks <jandylokc@gmail.com>
2024-12-23 05:29:27 +0100

    Update: docs, code optimisation

--- src/main/java/org/lts/lego_teh_set/events/GuildsCounter.java
+++ src/main/java/org/lts/lego_teh_set/events/GuildsCounter.java
@@ -1,18 +1,28 @@
-package org.lts.lego_teh_set.evenst;
+package org.lts.lego_teh_set.events;
 
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.api.events.guild.GuildLeaveEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import org.lts.lego_teh_set.LegoTehSetApplication;
+import org.lts.lego_teh_set.Bot;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
 
+/// Updates the bot status and writes the number of guilds in which the bot is located
+@Component
 public class GuildsCounter extends ListenerAdapter {
 
+    private final Bot bot;
+
+    @Autowired
+    public GuildsCounter(@Lazy Bot bot) {
+        this.bot = bot;
+    }
+
     private void updateGuildsLengthActivity() {
-        LegoTehSetApplication.getShard().setActivity(
-                Activity.watching(
-                        String.format("%s Guilds", LegoTehSetApplication.getShard()
+        bot.getShard().setActivity(Activity.watching(String.format("%s Guilds", bot.getShard()
                                 .getGuilds().size())));
     }
 
