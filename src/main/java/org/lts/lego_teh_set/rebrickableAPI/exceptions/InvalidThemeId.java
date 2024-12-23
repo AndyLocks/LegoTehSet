1d4a9882a7d0fb0f891342a51583a271f9c7fe65
AndyLocks <jandylokc@gmail.com>
2024-12-23 21:51:37 +0100

    Update: InvalidThemeId extends IllegalArgumentException

--- src/main/java/org/lts/lego_teh_set/rebrickableAPI/exceptions/InvalidThemeId.java
+++ src/main/java/org/lts/lego_teh_set/rebrickableAPI/exceptions/InvalidThemeId.java
@@ -1,6 +1,6 @@
 package org.lts.lego_teh_set.rebrickableAPI.exceptions;
 
-public class InvalidThemeId extends Exception {
+public class InvalidThemeId extends IllegalArgumentException {
 
     public InvalidThemeId(String errorMessage) {
         super(errorMessage);
