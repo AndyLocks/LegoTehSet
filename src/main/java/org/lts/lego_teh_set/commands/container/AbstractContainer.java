7ec599880d82e27f25842be9030d77141901c38b
AndyLocks <jandylokc@gmail.com>
2025-01-02 04:37:17 +0100

    Update: docs

--- src/main/java/org/lts/lego_teh_set/commands/container/AbstractContainer.java
+++ src/main/java/org/lts/lego_teh_set/commands/container/AbstractContainer.java
@@ -16,6 +16,24 @@ import java.util.List;
 /// in general anything that can be scrolled with arrows and
 /// anything that can contain information for display.
 ///
+/// # Implementation
+///
+/// It is important that the class can be serialized.
+///
+/// ## Redis
+///
+/// Implementations of this class are serialized and deserialized using [AbstractContainerRedisSerializer].
+///
+/// ### Redis configuration
+///
+/// - [org.lts.lego_teh_set.config.redis.AbstractContainerTemplateConfig]
+///
+/// ## Entry pint
+///
+/// There is only one entry point to manage all containers: [org.lts.lego_teh_set.events.AbstractContainerButtonsInteraction].
+/// It manages containers by first deserializing them from redis and then later putting them back in with
+/// [org.lts.lego_teh_set.config.redis.AbstractContainerTemplateConfig#TTL_DURATION] ttl duration.
+///
 /// @see ContainerDataProvider
 /// @see PageNavigable
 public abstract class AbstractContainer implements Serializable, PageNavigable, ContainerDataProvider {
